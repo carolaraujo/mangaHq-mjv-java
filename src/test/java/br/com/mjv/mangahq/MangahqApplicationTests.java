a019bfeac8f3b06783b72b5b79baf805b44a1ce7
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-10-23 11:24:56 -0300

    task 1 - adicionado arquivos base do spring

---
+++ src/test/java/br/com/mjv/mangahq/MangahqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mjv.mangahq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangahqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
