b7ce20cae91d7fcc70d9b93c9950bca57a87fad1
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-10-23 11:45:19 -0300

    task 1 - Adicionado classe modelo notícia

---
+++ src/main/java/br/com/mjv/mangahq/model/Noticia.java
@@ -0,0 +1,57 @@
+package br.com.mjv.mangahq.model;
+
+import java.util.Date;
+
+/**
+ * Classe representando a tabela TB_NOTICIAS
+ * @author kaique
+ *
+ */
+
+public class Noticia {
+	
+	private Long id;
+	private String titulo;
+	private String textoConteudo;
+	private String urlImagem;
+	private Date dataPublicacao;
+	private String autor;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTextoConteudo() {
+		return textoConteudo;
+	}
+	public void setTextoConteudo(String textoConteudo) {
+		this.textoConteudo = textoConteudo;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public Date getDataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setDataPublicacao(Date dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+}
