6a9c8da033c8cbe9294bfeaf7de11f2fa6873ada
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-10-24 16:22:38 -0300

    task 1 - Adicionado classes de repositório para notícia

---
+++ src/main/java/br/com/mjv/mangahq/dao/NoticiaRowMapper.java
@@ -0,0 +1,25 @@
+package br.com.mjv.mangahq.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import br.com.mjv.mangahq.model.Noticia;
+
+public class NoticiaRowMapper implements RowMapper<Noticia>{
+
+	@Override
+	public Noticia mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Noticia noticia = new Noticia();
+		noticia.setId(rs.getLong("id"));
+		noticia.setTitulo(rs.getString("titulo"));
+		noticia.setTextoConteudo(rs.getString("textoConteudo"));
+		noticia.setUrlImagem(rs.getString("urlImagem"));
+		noticia.setDataPublicacao(rs.getDate("dataPublicacao"));
+		noticia.setAutor(rs.getString("autor"));
+		noticia.setAcessos(rs.getLong("acessos"));
+		return noticia;
+	}
+
+}
