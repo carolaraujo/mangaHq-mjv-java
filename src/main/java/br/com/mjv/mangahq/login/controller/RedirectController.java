7e17580bfce9f15638c6cfc8dbf59c194d57095e
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-11-05 19:15:57 -0300

    task 2 - RedirectController, logger e javadoc

--- src/main/java/br/com/mjv/mangahq/login/controller/RedirectController.java
+++ src/main/java/br/com/mjv/mangahq/login/controller/RedirectController.java
@@ -1,5 +1,7 @@
 package br.com.mjv.mangahq.login.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -7,8 +9,17 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class RedirectController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	
+	/**
+	 * Método para redirecionar rota / para página inicial
+	 * @param atributos
+	 * @return
+	 */
 	@GetMapping("/")
 	public String redirectToLogin(RedirectAttributes atributos) {
+		LOGGER.info("RedirectController - Início do método @Get redirectToLogin");
+		LOGGER.info("RedirectController - Fim do método @Get redirectToLogin");
 		return "redirect:/mangahq";
 	}
 
